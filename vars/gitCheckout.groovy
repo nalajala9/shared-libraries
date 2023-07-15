35ece139b48fe8d44e362dfd0259054e83fd9e27
ravi <nalajalaravi99@gmail.com>
2023-07-15 18:57:10 +0530

    code added in gitCheckout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,1 +1,10 @@
 
+def call(String branch, String repourl) {
+    scmGit(
+        branches: [[name: branch]], 
+        extensions: [], 
+        userRemoteConfigs: [[url: url]]
+    )
+}
+
+call('main','https://github.com/nalajala9/maven-web.git')
